13c9bf2396ba7890d51ed5c3e949da74c298bb95
Simon de Haan <simon@praekeltfoundation.org>
2011-05-16 11:50:11 +0200

    storing in GAE datastore

--- ircarchive.py
+++ ircarchive.py
@@ -4,31 +4,64 @@ from google.appengine.ext.webapp.util import run_wsgi_app
 from google.appengine.ext.webapp import template
 from google.appengine.ext.webapp import xmpp_handlers
 from models import User, Channel, Message
-import json
+from django.utils import simplejson as json
 from datetime import datetime
+import logging
+
+"""
+sample json:
+
+{
+    "nickname": "sdehaan",
+    "server": "irc.freenode.net",
+    "channel": "#vumi",
+    "message_type": "message",
+    "message_content": "testing app engine",
+    "timestamp": "2011-05-16T08:17:53.749184"
+}
+
+"""
+
+TIMESTAMP_FORMAT = '%Y-%m-%dT%H:%M:%S.%f'
+
+def key(*args):
+    return db.Key.from_path(*args)
+
+def get_or_create(model, **kwargs):
+    # recreating get_or_insert, I must be doing something wrong
+    def txn():
+        key_name = key(model.kind(), '/'.join(kwargs.values())).id_or_name()
+        entity = model.get_by_key_name(key_name)
+        if entity is None:
+            entity = model(key_name=key_name, **kwargs)
+            entity.put()
+        return entity
+    return db.run_in_transaction(txn)
 
 class XmppHandler(xmpp_handlers.CommandHandler):
     def text_message(self, message=None):
         payload = json.loads(message.body)
         
         # get the payload data
-        username = payload.get('username')
+        nickname = payload.get('nickname')
         server = payload.get('server', 'unknown')
         channel = payload.get('channel')
         message_type = payload.get('message_type')
         message_content = payload.get('message_content')
-        timestamp = payload.get('timestamp')
+        timestamp = datetime.strptime(payload.get('timestamp'), TIMESTAMP_FORMAT)
         
+        user = get_or_create(User, server=server, nickname=nickname)
+        user.last_seen_at = datetime.utcnow()
+        user.put()
+        
+        channel = get_or_create(Channel, server=server, channel=channel)
         # store the message
-        msg = Message()
-        msg.user = User.find_by_username(server, username)
-        msg.channel = Channel.find_channel(server, channel)
-        msg.message_type = message_type
-        msg.message_content = message_content
-        msg.json = message.body
+        msg = Message(user=user, channel=channel, message_type=message_type,
+            message_content=message_content, json=message.body, 
+            timestamp=timestamp)
         msg.put()
         
-        msg.reply('thanks!')
+        message.reply('thanks!')
 
 application = webapp.WSGIApplication([
         ('/_ah/xmpp/message/chat/', XmppHandler)
--- models.py
+++ models.py
@@ -1,44 +1,21 @@
 from google.appengine.ext import db
 from datetime import datetime
 
-def key(*args):
-    return db.Key.from_path(*args)
-
 class User(db.Model):
     server = db.StringProperty(required=True)
     nickname = db.StringProperty(required=True)
-    created_at = db.DateTimeProperty(required, auto_now_add=True)
-    last_seen_at = db.DateTimeProperty(required)
+    created_at = db.DateTimeProperty(required=True, auto_now_add=True)
+    last_seen_at = db.DateTimeProperty(required=False)
     
-    @staticmethod
-    def find_by_nickname(server, nickname):
-        user = User.get(key(User.kind(), server, nickname))
-        if user is None:
-            user = User()
-            user.server = server
-            user.nickname = nickname
-            user.last_seen_at = datetime.now()
-            user.put()
-        return user
-
 class Channel(db.Model):
     channel = db.StringProperty(required=True)
     server = db.StringProperty(required=True)
     topic = db.TextProperty()
     
-    @staticmethod
-    def find_channel(server, channel):
-        channel = Channel.get(key(Channel.kind(), server, channel))
-        if channel is None:
-            channel = Channel()
-            channel.server = server
-            channel.channel = channel
-            channel.put()
-        return channel
 
 class Message(db.Model):
-    user = db.ReferencePropery(User)
-    channel = db.ReferencePropery(Channel)
+    user = db.ReferenceProperty(User)
+    channel = db.ReferenceProperty(Channel)
     timestamp = db.DateTimeProperty(required=True)
     message_type = db.StringProperty(required=True, choices=[
         "system", "action", "message"])
