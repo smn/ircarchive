f4f78b81d6ff23e62516e115a39408d19db8402d
Simon de Haan <simon@praekeltfoundation.org>
2011-05-16 16:46:37 +0200

    prefetching for performance

--- archive.py
+++ archive.py
@@ -2,6 +2,7 @@ from google.appengine.ext.webapp import template
 from google.appengine.ext import webapp
 from google.appengine.api import memcache
 from models import Message, Channel
+from utils import prefetch_refprops
 import os
 
 class ArchiveHandler(webapp.RequestHandler):
@@ -9,5 +10,6 @@ class ArchiveHandler(webapp.RequestHandler):
         channel_names = [channel.channel for channel in Channel.all()
                             if channel.channel.startswith('#')]
         messages = Message.all().order('-timestamp').fetch(1000)
+        prefetch_refprops(messages, Message.user)
         path = os.path.join(os.path.dirname(__file__), 'index.html')
         self.response.out.write(template.render(path, locals()))
--- utils.py
+++ utils.py
@@ -18,3 +18,12 @@ def parse_timestamp(timestamp):
     else:
         return datetime.strptime(timestamp, FORMAT)
 
+
+# http://blog.notdot.net/2010/01/ReferenceProperty-prefetching-in-App-Engine
+def prefetch_refprops(entities, *props):
+    fields = [(entity, prop) for entity in entities for prop in props]
+    ref_keys = [prop.get_value_for_datastore(x) for x, prop in fields]
+    ref_entities = dict((x.key(), x) for x in db.get(set(ref_keys)))
+    for (entity, prop), ref_key in zip(fields, ref_keys):
+        prop.__set__(entity, ref_entities[ref_key])
+    return entities
