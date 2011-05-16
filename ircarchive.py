87310bee42e30db59869f74e9a03426a87e68daa
Simon de Haan <simon@praekeltfoundation.org>
2011-05-16 09:57:01 +0200

    more work in progress

--- ircarchive.py
+++ ircarchive.py
@@ -5,12 +5,34 @@ from google.appengine.ext.webapp import template
 from google.appengine.ext.webapp import xmpp_handlers
 from models import User, Channel, Message
 import json
+from datetime import datetime
 
 class XmppHandler(xmpp_handlers.CommandHandler):
     def text_message(self, message=None):
         payload = json.loads(message.body)
+        
+        # get the payload data
+        username = payload.get('username')
+        server = payload.get('server', 'unknown')
+        channel = payload.get('channel')
+        message_type = payload.get('message_type')
+        message_content = payload.get('message_content')
+        timestamp = payload.get('timestamp')
+        
+        # store the message
+        msg = Message()
+        msg.user = User.find_by_username(username)
+        msg.channel = Channel.find_channel(server, channel)
+        msg.message_type = message_type
+        msg.message_content = message_content
+        msg.json = message.body
+        msg.put()
+        
+        msg.reply('thanks!')
 
-application = webapp.WSGIApplication([], debug=True)
+application = webapp.WSGIApplication([
+        ('/_ah/xmpp/message/chat/', XmppHandler)
+    ], debug=True)
 
 def main():
     run_wsgi_app(application)
--- models.py
+++ models.py
@@ -1,19 +1,44 @@
 from google.appengine.ext import db
+from datetime import datetime
+
+def key(*args):
+    return db.Key.from_path(*args)
 
 class User(db.Model):
     nickname = db.StringProperty(required=True)
     created_at = db.DateTimeProperty(required, auto_now_add=True)
     last_seen_at = db.DateTimeProperty(required)
+    
+    @staticmethod
+    def find_by_nickname(nickname):
+        user = User.get(key(User.kind(), nickname))
+        if user is None:
+            user = User()
+            user.nickname = nickname
+            user.last_seen_at = datetime.now()
+            user.put()
+        return user
 
 class Channel(db.Model):
     channel = db.StringProperty(required=True)
     server = db.StringProperty(required=True)
     topic = db.TextProperty()
+    
+    @staticmethod
+    def find_channel(server, channel):
+        channel = Channel.get(key(Channel.kind(), server, channel))
+        if channel is None:
+            channel = Channel()
+            channel.server = server
+            channel.channel = channel
+            channel.put()
+        return channel
 
 class Message(db.Model):
     user = db.ReferencePropery(User)
     channel = db.ReferencePropery(Channel)
-    timestamp = db.DateTimeProperty(required)
+    timestamp = db.DateTimeProperty(required=True)
     message_type = db.StringProperty(required=True, choices=[
         "system", "action", "message"])
     message_content = db.TextProperty(required=True)
+    json = db.TextProperty(required=True)
