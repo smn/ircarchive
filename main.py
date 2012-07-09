2973ccac526942485a1eb5cf608b2d28ed78533a
Simon de Haan <simon@praekeltfoundation.org>
2012-07-09 21:24:38 +0200

    moving to Django

--- main.py
+++ main.py
@@ -1,18 +1,5 @@
-from google.appengine.ext import db
-from google.appengine.ext import webapp
-from google.appengine.ext.webapp.util import run_wsgi_app
-from google.appengine.ext.webapp import template
-from xmpp import XmppHandler
-from archive import ArchiveHandler, ChannelHandler, BotHandler, EditChannelHandler
-import logging
+import os
+os.environ['DJANGO_SETTINGS_MODULE'] = 'ircarchive.settings'
 
-logger = logging.getLogger()
-logger.level = logging.DEBUG
-
-application = webapp.WSGIApplication([
-        (r'/_ah/xmpp/message/chat/$', XmppHandler),
-        (r'/channel/(.+)/(.+)/edit/$', EditChannelHandler),
-        (r'/channel/(.+)/(.+)/$', ChannelHandler),
-        (r'/bot/', BotHandler),
-        (r'/', ArchiveHandler),
-    ], debug=True)
+from django.core.handlers import wsgi
+app = wsgi.WSGIHandler()
